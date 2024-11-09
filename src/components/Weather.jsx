3755f8f3cbf86de804b2a5b148519616d57ed175
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-11-09 22:03:13 +0530

    Create components folder

---
+++ src/components/Weather.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import './Weather.css'
+import { FaSearch } from 'react-icons/fa'
+
+const Weather = () => {
+    const [city,setCity]=useState("");
+    const [weather , setWeather] = useState();
+    const [error , setError] = useState();
+    const API_KEY = "108d3779bf5c7a00803b52311109dc9e";
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+  function handleOnChange(event){
+    setCity(event.target.value);
+
+  }
+  async function fetchData() {
+    try{
+    let response =await fetch(url);
+    let output = await response.json();
+    if(response.ok){
+      setWeather(output);
+        setError('');
+    }else
+    {
+      setError('No data found :(  Please enter a valid city name ')
+    }
+    }
+    catch(error){
+
+    }
+  }
+  return (
+    <div className='container'>
+     <div className="city">
+        <input type="text" value={city} onChange={handleOnChange} placeholder='Enter any city name '/>
+        <button onClick={()=>fetchData()}>
+            <FaSearch ></FaSearch>
+        </button>
+     </div>
+     {
+      error && <p className='error-message'>{error}</p>
+     }
+    </div>
+  )
+}
+
+export default Weather
